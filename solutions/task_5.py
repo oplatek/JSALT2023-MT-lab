4cdbfb427156995606b3cba935e924631478fe48
Ondrej Platek <ondrej.platek@seznam.cz>
2023-06-13 16:37:58 +0200

    fix merging

--- solutions/task_5.py
+++ solutions/task_5.py
@@ -1,22 +1,26 @@
 import evaluate
 from comet import download_model, load_from_checkpoint
 
-predictions = [
-    "This is an examplary sentence.",
-    "Try different sentences for the reference and observe the change in scores.",
+source = [
+    "Dem Feuer konnte Einhalt geboten werden",
+    "Schulen und Kindergärten wurden eröffnet.",
 ]
-references = [
-    ["This is an examplary sentence."],
-    ["Try different sentences for the reference and observe the change in scores."],
+hypothesis = [
+    "The fire could be stopped",
+    "Schools and kindergartens were open",
+]
+reference = [
+    "They were able to control the fire.",
+    "Schools and kindergartens opened",
 ]
 
 comet = evaluate.load("comet")
 comet_score = comet.compute(
     predictions=hypothesis, references=reference, sources=source
 )
-print(round(results["score"], 1))
+print(comet_score)
 
-print("Notice the comet-qe-da metric does not use references!")
+print("\n\nNotice the comet-qe-da metric does not use references!")
 print("See Section 8 https://aclanthology.org/2020.wmt-1.101/")
 qe_model_path = download_model("wmt20-comet-qe-da")
 comet_qe_da = load_from_checkpoint(qe_model_path)
