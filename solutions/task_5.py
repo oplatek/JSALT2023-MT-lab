1807dd9b79917c2612f5ec9306082998a1117599
Ondrej Platek <ondrej.platek@seznam.cz>
2023-06-13 16:30:42 +0200

    add referenceless metric

--- solutions/task_5.py
+++ solutions/task_5.py
@@ -1,16 +1,25 @@
 import evaluate
-
-comet = evaluate.load("comet")
+from comet import download_model, load_from_checkpoint
 
 predictions = [
     "This is an examplary sentence.",
-    "Try different sentences for the reference and observe the change in scores."
+    "Try different sentences for the reference and observe the change in scores.",
 ]
 references = [
     ["This is an examplary sentence."],
-    ["Try different sentences for the reference and observe the change in scores."]
+    ["Try different sentences for the reference and observe the change in scores."],
 ]
 
-results = comet.compute(predictions=predictions, references=references)
-
+comet = evaluate.load("comet")
+comet_score = comet.compute(
+    predictions=hypothesis, references=reference, sources=source
+)
 print(round(results["score"], 1))
+
+print("Notice the comet-qe-da metric does not use references!")
+print("See Section 8 https://aclanthology.org/2020.wmt-1.101/")
+qe_model_path = download_model("wmt20-comet-qe-da")
+comet_qe_da = load_from_checkpoint(qe_model_path)
+data_no_ref = [{"src": src, "mt": mt} for src, mt in zip(source, hypothesis)]
+qe_comet_score = comet_qe_da.predict(data_no_ref)
+print(qe_comet_score)
